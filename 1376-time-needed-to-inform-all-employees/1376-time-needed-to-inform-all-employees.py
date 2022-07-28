a6d6a8bcd5c4976a34be8cab7d909883fd936062
Ananth Ravi Narayan <ananth360@gmail.com>
2022-07-28 01:09:12 -0400

    Time: 2127 ms (48.43%), Space: 49.5 MB (40.29%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        timeInformed = n * [10000000]
+        
+        # REMINDER: DO NOT DO n * [[ ]] as it creates n references to the SAME list
+        
+        adj = [ list() for i in range(n)]
+        for emp, man in enumerate(manager):
+            if man != -1:
+                adj[man].append(emp)
+        
+        def dfs(src, time) -> None:
+            timeInformed[src] = time
+            for emp in adj[src]:
+                dfs(emp, time + informTime[src])
+      
+        dfs(headID, 0)
+        
+        return max(timeInformed)
+        
