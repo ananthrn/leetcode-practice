084e512f770c3536ee13360d90872b9f5634a9ad
Ananth Ravi Narayan <ananth360@gmail.com>
2022-07-29 15:42:01 -0400

    Time: 4870 ms (13.95%), Space: 14.3 MB (61.67%) - LeetHub

---
+++ 839-similar-string-groups/839-similar-string-groups.py
@@ -0,0 +1,34 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        diff = [[x, y] for x, y in zip(s1, s2) if x != y]
+        return len(diff) == 2 and diff[0][::-1] == diff[1]
+    
+    def dfs(self, src: str, compNum: int, components: Dict[str, int]):
+        
+        def swap(string, i, j):
+            c = list(string)
+            c[i], c[j] = c[j], c[i]
+            return ''.join(c)
+        
+        components[src] = compNum
+        
+        # nextStrs = [swap(src, i, j) for i in range(len(src)) for j in range(i + 1, len(src))]
+        
+        for nextStr, nextNum in components.items():
+            if src != nextStr and nextNum == 0 and self.areAlmostEqual(src, nextStr):
+                self.dfs(nextStr, compNum, components)
+        
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        components = dict([(string, 0) for string in strs])
+        
+        compNum = 0
+        for string in strs:
+            if components[string] == 0:
+                compNum += 1
+                self.dfs(string, compNum, components)
+        
+        return compNum
+            
+            
+            
+        
