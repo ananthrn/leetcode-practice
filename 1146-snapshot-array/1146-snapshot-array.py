f97be69c1bc0b329d14340356320c98eee909213
Ananth Ravi Narayan <ananth360@gmail.com>
2022-07-29 02:00:52 -0400

    Time: 2764 ms (5.02%), Space: 140 MB (5.07%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -2,30 +2,29 @@ from sortedcontainers import SortedDict
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.array = [SortedDict() for i in range(length)]
-        for index in range(length):
-            self.array[index][0] = 0
+        self.array = [SortedDict([(0, 0)]) for i in range(length)]
+        # for index in range(length):
+        #     self.array[index][0] = 0
         self.snap_id = 0
         
     def set(self, index: int, val: int) -> None:
+        # set the latest snap id;s val
         self.array[index][self.snap_id] = val
         
 
     def snap(self) -> int:
+        # return the previous snap id
         val = self.snap_id
         self.snap_id += 1
         return val
 
     def get(self, index: int, snap_id: int) -> int:
-        
+        # if exists 
         if snap_id in self.array[index]:
             return self.array[index][snap_id]
-        
+        # else find the index of the previous snap id (since no updates were made to the index at this exact snap)
         prev_snap_index = self.array[index].bisect_left(snap_id) - 1
-        print("index, snap_id, prev_snap_index: ", index, snap_id, prev_snap_index)
-        print("array[index]: ", self.array[index])
         prev_snap_id, prev_value =  self.array[index].peekitem(prev_snap_index)
-        print("prev_snap_id, prev_value: ", prev_snap_id, prev_value)
         return prev_value
 
 # Your SnapshotArray object will be instantiated and called as such:
