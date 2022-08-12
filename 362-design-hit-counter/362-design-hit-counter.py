2c9d5c2b481774d330256329a197bd6cfa0c0708
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-12 17:31:18 -0400

    Time: 53 ms (37.87%), Space: 14.4 MB (15.43%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -1,17 +1,21 @@
 from collections import deque
+from sortedcontainers import SortedDict
 class HitCounter:
 
     def __init__(self):
         self.hits = deque()
         self.total = 0
     def hit(self, timestamp: int) -> None:
-        self.hits.append(timestamp)
+        if self.hits and self.hits[-1][0] == timestamp:
+            self.hits[-1][1] += 1
+        else:
+            self.hits.append([timestamp, 1])
         self.total+=1
 
     def getHits(self, timestamp: int) -> int:
-        while self.hits and self.hits[0]<= timestamp - 300:
+        while self.hits and self.hits[0][0]<= timestamp - 300:
             val = self.hits.popleft()
-            self.total -= 1
+            self.total -= val[1]
         return self.total
         
 
