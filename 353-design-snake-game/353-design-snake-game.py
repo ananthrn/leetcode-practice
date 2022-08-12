b3853d88a5788149973a3233d99343cc965f3677
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-12 17:17:51 -0400

    Time: 1000 ms (5.14%), Space: 15.9 MB (56.36%) - LeetHub

---
+++ 353-design-snake-game/353-design-snake-game.py
@@ -0,0 +1,56 @@
+from collections import deque
+class SnakeGame:
+    
+    
+    
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        self.food = deque([tuple(fooditem) for fooditem in food])
+        self.width = width
+        self.height = height
+        self.snake = deque([(0, 0)])
+        self.dirs = {
+            "R": (0, 1),
+            "L": (0, -1),
+            "U": (-1, 0),
+            "D": (1, 0)
+        }
+        print("food: ", food)
+        
+
+    def move(self, direction: str) -> int:
+        headPos = self.snake[0]
+        nextPos = (headPos[0] + self.dirs[direction][0], headPos[1] + self.dirs[direction][1])
+        
+        if not ( 0 <= nextPos[0] < self.height) or not( 0 <= nextPos[1] < self.width):
+            print(f"{nextPos} out of bounds")
+            return -1
+        
+        
+        
+        
+        print("Current self.snake: ", self.snake)
+        foodFound = self.food and nextPos == self.food[0]
+        if foodFound:        
+            self.food.popleft()
+        else:
+            self.snake.pop()
+        
+        if nextPos in self.snake:
+            print(f"{nextPos} already in snake: {self.snake}")
+            return -1
+        self.snake.appendleft(nextPos)
+        # self.food.discard(nextPos) 
+        print("headPos: ", headPos)
+        print("dir: ", direction)
+        print("nextPos: ", nextPos)
+        print("self.food", self.food)
+        print("foodFound: ", foodFound)
+        # print("Current self.snake: ", self.snake)
+        print("Final self.snake: ", self.snake)
+        print()
+        return len(self.snake) - 1
+            
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
