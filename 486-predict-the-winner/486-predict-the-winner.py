b31f4a6384b8fd25754f9fc494a75d9aba6eaaef
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-12 23:31:19 -0400

    Time: 7150 ms (7.68%), Space: 115.2 MB (7.78%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -5,13 +5,18 @@ class Solution:
         turn = 0
         start = 0
         end = len(nums) - 1
+        
+        mem = {}
         @cache
         def dfs(start, end, turn, scores) -> int:
+            
             if start == end:
                 scoresNew = list(scores)
                 scoresNew[turn] += nums[start]
-                # print("here: ")
                 return 0 if scoresNew[0] >= scoresNew[1] else 1
+            
+            if (start, end, turn, scores[0], scores[1]) in mem:
+                return mem.get((start, end, turn, scores[0], scores[1]))
             
             startScores = list(scores)
             startScores[turn] += nums[start]
@@ -20,8 +25,9 @@ class Solution:
             endScores = list(scores)
             endScores[turn] += nums[end]
             checkEnd = dfs(start, end -1, 1 - turn, tuple(endScores))
-             
             
+            ans = turn if (checkStart == turn or checkEnd == turn) else 1 - turn
+            mem[(start, end, turn, scores[0], scores[1])] = ans
             if checkStart == turn or checkEnd == turn:
                 return turn
             else:
