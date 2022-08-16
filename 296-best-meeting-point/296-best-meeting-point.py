aee2776c5e0e0e668430e5e987230f55a7f1b080
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-16 18:23:34 -0400

    Time: 147 ms (76.23%), Space: 15.8 MB (29.15%) - LeetHub

--- 296-best-meeting-point/296-best-meeting-point.py
+++ 296-best-meeting-point/296-best-meeting-point.py
@@ -6,14 +6,9 @@ class Solution:
         y_pos = sorted([c for r in range(m) for c in range(n) if grid[r][c] == 1])
         
         med_x = x_pos[(len(x_pos) - 1)//2]
-        # med_x_plus_1 = x_pos[(len(x_pos) - 1)//2 + 1]
         
         med_y = y_pos[(len(y_pos) - 1)//2]
-        # med_y_plus_1 = y_pos[(len(y_pos) - 1)//2 + 1]
         
-        # min_x = min(sum([abs(x - med_x) for x in x_pos]), sum([abs(x - med_x_plus_1) for x in x_pos]))
-        
-        # min_y = min(sum([abs(y - med_y) for y in y_pos]), sum([abs(y - med_y_plus_1) for y in y_pos]))
         min_x = sum([abs(x - med_x) for x in x_pos])
         min_y = sum([abs(y - med_y) for y in y_pos])
         return min_x + min_y
