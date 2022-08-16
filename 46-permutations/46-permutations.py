ee1ad4d99f605e1ef128049aebba965fec51c00e
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-16 19:10:53 -0400

    Time: 36 ms (98.51%), Space: 14 MB (58.59%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,5 +1,25 @@
 from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return permutations(nums)
+        permutations = set()
+        seen = len(nums) * [False]
+        perm = []
+        def backtrack():
+            if len(perm) == len(nums):
+                permutations.add(tuple(perm))
+                return
+            
+            for i in range(len(nums)):
+                if not seen[i]:
+                    perm.append(nums[i])
+                    seen[i] = True
+                    backtrack()
+                    seen[i] = False
+                    perm.pop()
+                    
+        backtrack()
+        
+        return list(permutations)
+        
+        
         
