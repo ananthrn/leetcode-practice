c2ba941a63e54d003e5cc01eab11467ff3100076
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-26 16:42:08 -0400

    Time: 469 ms (19.97%), Space: 23 MB (18.82%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.py
+++ 491-increasing-subsequences/491-increasing-subsequences.py
@@ -1,18 +1,18 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         ans = set()
-        
-        def backtrack(ind, cur):
+        cur = []
+        def backtrack(ind):
             if len(cur) >= 2:
                 ans.add(tuple(cur))
             
             for nxtInd in range(ind + 1, len(nums)):
                 if ind == -1 or nums[nxtInd] >= nums[ind]:
                     cur.append(nums[nxtInd])
-                    backtrack(nxtInd, cur)
+                    backtrack(nxtInd)
                     cur.pop()
         
-        backtrack(-1, [])
+        backtrack(-1)
         
         return list(ans)
         
