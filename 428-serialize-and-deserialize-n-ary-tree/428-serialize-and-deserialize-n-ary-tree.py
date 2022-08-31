9682e9b723d41c55af49e65c5e20d8ee840a296f
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-31 00:25:19 -0400

    Time: 179 ms (11.32%), Space: 17.6 MB (18.67%) - LeetHub

---
+++ 428-serialize-and-deserialize-n-ary-tree/428-serialize-and-deserialize-n-ary-tree.py
@@ -0,0 +1,79 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+from collections import deque
+class Codec:
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+        
+        :type root: Node
+        :rtype: str
+        """
+        data = ""
+        def serializeHelper(rootNode: 'Node') -> None:
+            nonlocal data
+            print("BEGIN: ")
+            print("rootNode: ", rootNode)
+            # print("data: ", data)
+            if rootNode is None:
+                data += " #"
+            else:
+                
+                data += " " + str(rootNode.val)
+                print("rootNode.val: ", rootNode.val)
+                if rootNode.children is not None:
+                    print("children: ", [child.val if child is not None else "None" for child in rootNode.children])
+                    print()
+                    numChildren = len(rootNode.children)
+                    data += " " + str(numChildren)
+                    
+                    if numChildren > 0:
+                        for child in rootNode.children:
+                            serializeHelper(child)
+                else:
+                    data += " " + "0"
+        
+        
+        serializeHelper(root)
+        
+        print("data: ", data)
+        return data
+	
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: Node
+        """
+        dataList = deque(data.split())
+        print("GOT TO DESERIALIZE: ")
+        def deserializeHelper() -> 'Node':
+            nonlocal dataList
+            strVal = dataList.popleft()
+            
+            
+            if strVal == "#":
+                return None
+            else:
+                val = int(strVal)
+                numChildren = int(dataList.popleft())
+                
+                if numChildren == 0:
+                    return Node(val=val, children=[])
+                else:
+                    children = [deserializeHelper() for childNum in range(numChildren)]
+                    return Node(val=val, children=children)
+            
+            return None
+        
+        return deserializeHelper()
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
