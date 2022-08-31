8adeea7e7afaa4bf9d7d31a55e598f05c755f947
Ananth Ravi Narayan <ananth360@gmail.com>
2022-08-30 23:50:06 -0400

    Time: 815 ms (21.44%), Space: 36.9 MB (5.08%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -0,0 +1,22 @@
+import numpy as np
+class Solution:
+
+    def __init__(self, w: List[int]):
+        np.random.seed(42)
+        self.prefWeights = list(w)
+        for i in range(1, len(w)):
+            self.prefWeights[i] += self.prefWeights[i-1]
+        
+        
+    def pickIndex(self) -> int:
+        randomVal = self.prefWeights[-1] * np.random.rand()
+        
+        index = bisect_left(self.prefWeights, randomVal)
+        
+        return index
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
