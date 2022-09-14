d489480f21012bf8a9f39da3f5a559f78ce4cea8
Ananth Ravi Narayan <ananth360@gmail.com>
2022-09-14 01:15:19 -0400

    Time: 1560 ms (66.82%), Space: 25.3 MB (22.70%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
@@ -7,30 +7,24 @@ class Solution:
         
         for i, string in enumerate(dic):
             for char in string:
-                hashes[i] = (26 * (hashes[i]) + (ord(char) - ord('a'))) % MOD
+                hashes[i] = ((27 * hashes[i])%MOD + (ord(char) - ord('a'))) % MOD
         
-#         seen = set()
-#         for i, string in enumerate(dic):
-#             BASE = 1
-#             for char in reversed(string):
-#                 new_hash = (hashes[i] - ((ord(char) - ord('a')) * BASE)%MOD + MOD) % MOD
-#                 if new_hash in seen:
-#                     return True
-#                 seen.add(new_hash)
-                
-#                 BASE = (BASE * 26) % MOD
-                
-                
-        base = 1
-        for j in range(n - 1, -1, -1):        
-            seen = set()
-            for i in range(m):
-                new_h = (hashes[i] - base * (ord(dic[i][j]) - ord('a'))) % MOD
-                if new_h in seen:
+        seen = set()
+        for i, string in enumerate(dic):
+            BASE = 1
+            for char_ind in range(n - 1, -1, -1):
+                char = string[char_ind]
+                new_hash = (hashes[i] - ((ord(char) - ord('a') + 26) * BASE)  + MOD) % MOD
+                if new_hash in seen:
+                    # print("i, string: ", i, string)
+                    # print("hashes[i]: ", hashes[i])
+                    # print("char_ind: ", char_ind)
+                    # print("char: ", char)
+                    # print("new_hash: ", new_hash)
+                    # print("BASE: ", BASE)
                     return True
-                seen.add(new_h)
-            base = 26 * base % MOD
+                seen.add(new_hash)
+                
+                BASE = (BASE * 27) % MOD
         
         return False
-                
-        
