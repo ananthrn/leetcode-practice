b92f7dbe715c7443bc3b899682a04e05208893ef
Ananth Ravi Narayan <ananth360@gmail.com>
2022-09-14 01:27:43 -0400

    Time: 232 ms (39.24%), Space: 15.3 MB (15.65%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,38 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        seen = set()
+        def isClosedIsland(r, c) -> bool:
+            nonlocal seen
+            Q = deque([(r, c)])
+            seen.add((r, c))
+            
+            reachedSide = False
+            while len(Q) > 0:
+                tp_r, tp_c = Q.pop()
+                if tp_r in [0, m - 1] or tp_c in [0, n - 1]:
+                    reachedSide = True
+                    
+                for nxt_r, nxt_c in (
+                    (tp_r + 1, tp_c),
+                    (tp_r - 1, tp_c),
+                    (tp_r, tp_c + 1),
+                    (tp_r, tp_c - 1),
+                ):
+                    if 0 <= nxt_r < m and 0 <= nxt_c < n:
+                        if (nxt_r, nxt_c) not in seen and grid[nxt_r][nxt_c] == 0:
+                            seen.add((nxt_r, nxt_c))
+                            Q.appendleft((nxt_r, nxt_c))
+            
+            return reachedSide == False
+        
+        count = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 0 and (r,c) not in seen and isClosedIsland(r, c):
+                    count += 1
+        
+        return count
+                    
+                            
+        
