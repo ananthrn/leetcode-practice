1f140fda52534b8b9e8072697e5f7145ef8f5cde
Ananth Ravi Narayan <ananth360@gmail.com>
2022-09-15 01:27:23 -0400

    Time: 3608 ms (8.21%), Space: 33.3 MB (21.38%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -0,0 +1,24 @@
+from collections import Counter
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed = sorted(changed)
+        cnt = Counter(changed)
+        
+        ans = []
+        for elem in changed:
+            if elem == 0:
+                if cnt[elem] %2 !=0:
+                    return []
+                
+                ans += cnt[elem]//2 * [elem]
+                cnt[elem] = 0
+            elif elem > 0:
+                currentCount = cnt[elem]
+                if cnt[2*elem] < currentCount:
+                    return []
+                ans += currentCount * [elem]
+                cnt[elem] = 0
+                cnt[2 * elem] -= currentCount
+        
+        return ans
+        
