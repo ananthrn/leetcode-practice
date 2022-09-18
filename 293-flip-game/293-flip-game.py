ce6fb0a131a4ea802fb35c00ddd3f822ca298e35
Ananth Ravi Narayan <ananth360@gmail.com>
2022-09-18 01:19:38 -0400

    Time: 35 ms (86.72%), Space: 14.1 MB (50.55%) - LeetHub

--- 293-flip-game/293-flip-game.py
+++ 293-flip-game/293-flip-game.py
@@ -5,7 +5,7 @@ class Solution:
         }
         
         ans = [
-            currentState[0:j] + flipDict[currentState[j:j+2]] + currentState[j+2:] for j in range(len(currentState)-1) if currentState[j:j+2] in flipDict
+            currentState[0:j] + "--" + currentState[j+2:] for j in range(len(currentState)-1) if currentState[j:j+2] == "++"
         ]
         
         return ans
