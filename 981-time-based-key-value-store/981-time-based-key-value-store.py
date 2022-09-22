6b063c11efed8af1e5a71adc5735e90294296b08
Ananth Ravi Narayan <ananth360@gmail.com>
2022-09-22 02:41:01 -0400

    Time: 876 ms (80.73%), Space: 72.5 MB (20.09%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        self.map = defaultdict(SortedList)
+        self.map = defaultdict(lambda: SortedList(key = lambda x: x[0]))
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.map[key].add((timestamp, value))
@@ -14,16 +14,10 @@ class TimeMap:
             return ""
         
         index = self.map[key].bisect_left((timestamp + 1, "ZZZZZZZZZZZZZZZZZZZZZ")) - 1
-        # print("self.map[key]: ", self.map[key])
-        # print("timestamp: ", timestamp)
-        # print("index: ", index)
         if index >= 0 and index < len(self.map[key]):
-            # print("ans: ", self.map[key][index][1])
-            # print()
+
             return self.map[key][index][1]
         else:
-            # print("ans: ", "")
-            # print()
             return ""
 
 
