c1cf14d842ae59a65aa2cda953897c59f4625ccf
Ananth Ravi Narayan <ananth360@gmail.com>
2022-09-22 23:08:34 -0400

    Time: 3062 ms (40.16%), Space: 13.9 MB (80.31%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,23 +1,9 @@
 class Solution:
     
     def concatenatedBinary(self, n: int) -> int:
-        def powMod(base, exp, MOD) -> int:
-            if exp == 0:
-                return 1
-            if exp == 1:
-                return base % MOD
-            
-            ansHalf = powMod(base, exp//2, MOD)
-            ans = (ansHalf * ansHalf) % MOD
-            
-            if exp % 2 == 1:
-                return (ans * base)%MOD
-            else:
-                return ans 
         
         MOD = 10 **9 +7
         binPow = 18 * [1]
-        # binPow[1] = ÷
         
         for exp in range(1, 18):
             binPow[exp] = (binPow[exp- 1] * 2)%MOD
