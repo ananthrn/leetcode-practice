9dbb43f0e1e5210d260423703cbbf3589c49cd08
Ananth Ravi Narayan <ananth360@gmail.com>
2022-12-23 13:34:31 -0500

    Time: 189 ms (97.28%), Space: 17.2 MB (73.64%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        # Key idea: diagonal nubmer is 
+        m, n = len(mat), len(mat[0])
+        diagNumToElements = collections.defaultdict(list)
+        
+        for r in range(m):
+            for c in range(n):
+                # Append element to the appropriate diagonal number
+                diagNumToElements[r + c].append(mat[r][c])
+        
+        
+        ans = []
+        
+        for diagNum, elems in diagNumToElements.items():
+            if diagNum%2 == 0:
+                ans += elems[::-1]
+            else:
+                ans += elems
+        
+        return ans
