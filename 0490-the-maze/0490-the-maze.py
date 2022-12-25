0780fefa2e933f815f19ec0bf71cacae4867df90
Ananth Ravi Narayan <ananth360@gmail.com>
2022-12-24 22:22:43 -0500

    Time: 392 ms (72.23%), Space: 14.7 MB (46.77%) - LeetHub

---
+++ 0490-the-maze/0490-the-maze.py
@@ -0,0 +1,53 @@
+from collections import deque
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        def getNextPos(r, c, direction):
+            dir_r, dir_c = dirMap[direction]
+            
+            while 0 <= r < m and 0 <= c < n and maze[r][c] == 0:
+                r += dir_r
+                c += dir_c
+            r -= dir_r
+            c -= dir_c
+            
+            return (r, c)
+        
+        m, n = len(maze), len(maze[0])
+        start_r, start_c = start
+        
+        dirMap = {
+            'L': (0, -1),
+            'R': (0, 1),
+            'U': (-1, 0),
+            'D': (1, 0),
+        }
+        
+        Q = deque()
+        seen = set()
+        
+        Q.append((0, start_r, start_c))
+        # seen.add((start_r, start_c))
+        
+        while len(Q) > 0:
+            print("steps, r, c:", Q[-1])
+            steps, r, c = Q.pop()
+            
+            if (r, c) == tuple(destination):
+                return True
+            
+            if (r, c) not in seen:
+                seen.add((r, c))
+                for direction in dirMap:
+                    nxt_r, nxt_c = getNextPos(r, c, direction)
+                    print("direction: ", direction)
+                    print("nxt_r, nxt_c: ", nxt_r, nxt_c)
+                    print()
+                    if 0 <= nxt_r < m and 0 <= nxt_c < n and (nxt_r, nxt_c) not in seen:
+                        Q.appendleft((steps + 1, nxt_r, nxt_c))
+        
+        return False
+                
+                
+                
+                            
+                            
