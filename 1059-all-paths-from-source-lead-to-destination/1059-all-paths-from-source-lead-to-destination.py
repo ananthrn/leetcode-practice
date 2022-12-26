8a92e1d80c4b5b6c18bbb60ad98e8e01ccf21a2d
Ananth Ravi Narayan <ananth360@gmail.com>
2022-12-26 15:05:37 -0500

    Time: 334 ms (72.49%), Space: 20.2 MB (38.29%) - LeetHub

---
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -0,0 +1,43 @@
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        seen = collections.defaultdict(int)
+        destinations = []
+        
+        adj  = collections.defaultdict(list)
+        
+        for u, v in edges:
+            adj[u].append(v)
+            # adj[v].append(u)
+         
+        def getDestinations(src: int) -> bool:
+            # also check for cycles:
+            
+            seen[src] = 1
+            
+            if len(adj[src]) == 0:
+                destinations.append(src)
+                seen[src] = 2
+                return True
+            
+            for nxt in adj[src]:
+                if nxt == src or seen[nxt] == 1:
+                    # cycle
+                    return False
+                elif seen[nxt] == 0:
+                    check = getDestinations(nxt)
+                    if not check:
+                        return False
+            
+            seen[src] = 2
+            
+            return True
+        
+        check = getDestinations(source)
+        
+        print("check: ", check)
+        print("destinations: ", destinations)
+        return check and destinations == [destination]
+        
+                
+        
