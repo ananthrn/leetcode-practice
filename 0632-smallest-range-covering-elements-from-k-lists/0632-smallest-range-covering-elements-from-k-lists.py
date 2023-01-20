bd366e27f8e99ca68817f142b38dd4c1098cb2c6
Ananth Ravi Narayan <ananth360@gmail.com>
2023-01-20 13:28:13 -0500

    Time: 233 ms (86.45%), Space: 20.3 MB (80.52%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -4,12 +4,12 @@ class Solution:
             return list1[1] - list1[0] < list2[1] - list2[0] or ((list1[1] - list1[0] == list2[1] - list2[0]) and list1[0] < list2[0])
         
         heap = [
-            [lis[0], lisIndex, 0] for lisIndex, lis in enumerate(nums)
+            (lis[0], lisIndex, 0) for lisIndex, lis in enumerate(nums)
         ]
         
         heapq.heapify(heap)
         
-        ans = [-10e9, 10e9]
+        ans = -10e9, 10e9
         
         right = max(val[0] for val in heap)
         
@@ -18,11 +18,11 @@ class Solution:
             left, i, j = heapq.heappop(heap)
             
             if right - left < ans[1] - ans[0]:
-                ans = [left, right]
+                ans = left, right
             
             if j + 1 < len(nums[i]):
                 right = max(right, nums[i][j+1])
-                heapq.heappush(heap, [nums[i][j+1], i, j + 1])
+                heapq.heappush(heap, (nums[i][j+1], i, j + 1))
             else:
                 return ans
             
