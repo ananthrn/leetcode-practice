7a3a0b497f7db6a5755e6186c6213cfe2311f5dc
Ananth Ravi Narayan <ananth360@gmail.com>
2023-01-25 13:42:29 -0500

    Time: 31 ms (74.96%), Space: 13.9 MB (43.21%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -3,7 +3,5 @@ class Solution:
         bin_n = bin(n)
         
         new_val = list(map(lambda b: str(1 - int(b)), bin_n[2:]))
-        
-        bin_complement = "0b" + "".join(new_val)
-        
+                
         return int("".join(new_val), 2)
