7f4e26047c905d6c66750a1df551d2b657124bb6
Ananth Ravi Narayan <ananth360@gmail.com>
2023-01-27 13:04:24 -0500

    Time: 54 ms (91.35%), Space: 13.8 MB (92.71%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,17 +1,6 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        
-        def compare(word1, word2):
-            if cnt[word1] != cnt[word2]:
-                return cnt[word2] - cnt[word1]
-            else:
-                if word1 < word2:
-                    return -1
-                elif word2 > word1:
-                    return 1
-                else:
-                    return 0
-                
+           
         cnt = Counter(words)
         
         sortedWords = sorted(cnt.keys(), key=lambda word: (-cnt[word], word))
