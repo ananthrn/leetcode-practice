3bc421cb28e53584a7fbb9eb3175a3407acdca76
Ananth Ravi Narayan <ananth360@gmail.com>
2023-01-30 12:23:55 -0500

    Time: 55 ms (88.97%), Space: 13.9 MB (59.15%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,8 +1,23 @@
+class Pair:
+    def __init__(self, word: str, freq: int):
+        self.freq = freq
+        self.word = word
+    
+    def __lt__(self, p):
+        return self.freq < p. freq or (self.freq == p.freq) and (self.word > p.word)
+    
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
            
         cnt = Counter(words)
         
-        sortedWords = sorted(cnt.keys(), key=lambda word: (-cnt[word], word))
+        minHeap = []
+        for word, freq in cnt.items():
+            heapq.heappush(minHeap, Pair(word, freq))
+            
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
         
-        return sortedWords[:k]
+        ans = [heapq.heappop(minHeap).word for _ in range(len(minHeap))]
+        
+        return ans[::-1]
