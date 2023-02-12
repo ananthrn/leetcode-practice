3d59c3d30ca942e3011d1d55379e3ddc34627703
Ananth Ravi Narayan <ananth360@gmail.com>
2023-02-11 22:04:12 -0500

    Time: 50 ms (61.86%), Space: 14.7 MB (16.55%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,30 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        costMap = {
+            0: 1,
+            1: 7,
+            2: 30
+        }
+        
+        @cache
+        def helper(index):
+            if index >= len(days):
+                return 0
+            
+            mincost = 1_000_000_000
+            for costIndex, cost in enumerate(costs):
+                dayPass = costMap[costIndex]
+                nextIndex = bisect.bisect_right(days, days[index] + dayPass - 1, index+1)
+                mincost = min(mincost, cost + helper(nextIndex))
+            
+            print("index:", index)
+            print("mincost: ", mincost)
+            print()
+            return mincost
+        
+        
+        return helper(0)
+            
+            
+        
