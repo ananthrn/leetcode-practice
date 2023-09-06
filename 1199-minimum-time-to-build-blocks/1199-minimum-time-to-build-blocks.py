1303b46b859c40657ba7f9cf052210cf9080dd60
Ananth Ravi Narayan <ananth360@gmail.com>
2023-09-06 14:13:01 -0400

    Time: 76 ms (61.28%), Space: 16.4 MB (87.16%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.py
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.py
@@ -1,29 +1,12 @@
 class Solution:
     def minBuildTime(self, blocks: List[int], split: int) -> int:
         
-        cache = [[None] * (len(blocks) + 1) for _ in range(len(blocks) + 1)]
+        heapq.heapify(blocks)
         
-        blocks = sorted(blocks, reverse=True)
-        # @lru_cache(maxsize=1000000)
-        def backtrack(index: int, numWorkers: int) -> int:
-            if index >= len(blocks):
-                return 0
+        while len(blocks) > 1:
+            largest = heapq.heappop(blocks)
+            second_largest = heapq.heappop(blocks)
+            heapq.heappush(blocks, second_largest + split)
             
-            if numWorkers <= 0:
-                return inf
-            
-            workersNeeded = len(blocks) - index
-            
-            if numWorkers >= workersNeeded:
-                return blocks[index]
-            
-            if cache[index][numWorkers] != None:
-                return cache[index][numWorkers]
-        
-            cache[index][numWorkers] = min(split + backtrack(index, min(2 * numWorkers, workersNeeded)), max(blocks[index], backtrack(index + 1, numWorkers - 1)))
-            
-            return cache[index][numWorkers]            
-        
-        ans = backtrack(0, 1)
-        
-        return ans
+        return heapq.heappop(blocks)
+        # return ans
