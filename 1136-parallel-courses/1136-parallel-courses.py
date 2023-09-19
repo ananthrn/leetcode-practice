b7eb46051086f28c7498ebc5d534191222865696
Ananth Ravi Narayan <ananth360@gmail.com>
2023-09-19 14:58:28 -0400

    Time: 260 ms (27.48%), Space: 20 MB (59.02%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.py
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -1,54 +1,37 @@
-from collections import defaultdict
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        seen = [0] * (n + 1)
+        semester = [None] * (n + 1)
         
-        prereq = defaultdict(list)
+        adj = collections.defaultdict(list)
         
-        for a, b in relations:
-            prereq[b].append(a)
-        
-        
-        seen = (n +1) * [0]
-        
-        semester = dict()
-        
-        counter = 0
-        
-        def dfsHelper(src: int) -> Tuple[Union[int, bool]]:
-            """
-            returns False if cycle is found
-            """
-            nonlocal counter, seen, semester, prereq
-            seen[src] = 1
-            ans = 0
-            for v in prereq[src]:
-                # print("src, v: ", src, v)
-                # print("seen[v]: ", seen[v])
-                if seen[v] == 1:
-                    # Cycle detected
-                    return False
-                elif seen[v] == 0:
-                    checkNotCyle = dfsHelper(v)
-                    if not checkNotCyle:
-                        return False
-                ans = max(ans, semester[v])
-                    
-            seen[src] = 2
-            semester[src] = ans + 1
-            counter += 1
+        for prereq, nextCourse in relations:
+            adj[nextCourse].append(prereq)
             
+        def dfs(src: int) -> bool:
+            seen[src] = 1
+            
+            semester[src] = 1
+            for nxt in adj[src]:
+                if seen[nxt] == 1:
+                    return False
+                elif seen[nxt] == 2:
+                    semester[src] = max(semester[src], semester[nxt] + 1)
+                else:
+                    check = dfs(nxt)
+                    if not check:
+                        return False
+                    semester[src] = max(semester[src], semester[nxt] + 1)
+            
+            seen[src] = 2
             return True
         
+        for i in range(1, n + 1):
+            if seen[i] == 0:
+                check = dfs(i)
+            if not check:
+                return -1
         
-        for src in range(1, n + 1):
-            if seen[src] == 0:
-                counter = 1
-                checkNotCyle  = dfsHelper(src)
-                if not checkNotCyle:
-                    return -1
-        
-        # print("semester: ", semester)
-        return max(semester.values())
-            
+        return max(sem for sem in semester if sem is not None)
             
         
