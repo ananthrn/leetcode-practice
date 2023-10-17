7403f4311820da1f0d4d4140f27e5983326b2bd2
Ananth Ravi Narayan <ananth360@gmail.com>
2023-10-17 14:58:31 -0700

    Time: 247 ms (45.57%), Space: 16.7 MB (5.70%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -0,0 +1,62 @@
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        minLeft = list(range(n))
+        minRight = list(range(n))
+        
+        maxLeft = list(range(n))
+        maxRight = list(range(n))
+        
+        for ind in range(n):
+            for j in reversed(range(ind)):
+                if nums[ind] < nums[j]:
+                    minLeft[ind] = j
+                else:
+                    break
+            
+            for j in reversed(range(ind)):
+                if nums[ind] > nums[j]:
+                    maxLeft[ind] = j
+                else:
+                    break
+            
+            for j in range(ind + 1, n):
+                if nums[ind] <= nums[j]:
+                    minRight[ind] = j
+                else:
+                    break
+            
+            for j in range(ind + 1, n):
+                if nums[ind] >= nums[j]:
+                    maxRight[ind] = j
+                else:
+                    break
+        
+        print("minLeft: ", minLeft)
+        print("minRight: ", minRight)
+        
+        print("maxLeft: ", maxLeft)
+        print("maxRight: ", maxRight)
+        
+        ans = 0 
+        
+        for ind in range(n):
+            
+            maxWindow = (ind - maxLeft[ind] + 1) * (maxRight[ind] - ind + 1)
+            minWindow = (ind - minLeft[ind] + 1) * (minRight[ind] - ind + 1)
+            thisAnswerPositive = nums[ind] * maxWindow
+            thisAnswerNegative = nums[ind] * minWindow
+            
+            print("ind, nums[ind]: ", ind, nums[ind])
+            print("maxWindow: ", maxWindow)
+            print("minWindow: ", minWindow)
+            print("thisAnswerPositive: ", thisAnswerPositive)
+            print("thisAnswerNegative: ", thisAnswerNegative)
+            print()
+            ans += nums[ind] * ( maxWindow -  minWindow)
+        
+        
+        
+        return ans
+        
