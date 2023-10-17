efa6fe8dd87cf609691f685ad272dc64d97a94e6
Ananth Ravi Narayan <ananth360@gmail.com>
2023-10-16 17:58:01 -0700

    Time: 1261 ms (15.39%), Space: 180.6 MB (8.97%) - LeetHub

---
+++ 2313-minimum-flips-in-binary-tree-to-get-result/2313-minimum-flips-in-binary-tree-to-get-result.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minimumFlips(self, root: Optional[TreeNode], result: bool) -> int:
+        opMap = {
+            2: lambda x, y: x or y,
+            3: lambda x, y: x and y,
+            4: lambda x, y: x != y,
+            5: lambda x: not x
+        }
+        
+        @cache
+        def minFlipsHelper(node: Optional[TreeNode], result: bool) -> int:
+            # print("node.val", node.val)
+            if node.left is not None and node.right is not None:
+                minFlips = 1000000
+                for leftVal in [True, False]:
+                    for rightVal in [True, False]:
+                        if opMap[node.val](leftVal, rightVal) == result:
+                            minFlips = min(minFlips, minFlipsHelper(node.left, leftVal) + minFlipsHelper(node.right, rightVal))
+                return minFlips
+                
+            
+            elif node.left is not None:
+                assert node.val == 5, "not a not node"
+                
+                return minFlipsHelper(node.left, not result)
+            elif node.right is not None:
+                assert node.val == 5, "not a not node"
+                return minFlipsHelper(node.right, not result)
+            else:
+                return 1 if node.val != result else 0
+        
+        return minFlipsHelper(root, result)
+            
+                
+            
+            
