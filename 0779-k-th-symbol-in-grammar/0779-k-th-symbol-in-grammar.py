032450534ae3b0368dc06fe0797445d71175c99c
Ananth Ravi Narayan <ananth360@gmail.com>
2023-10-25 09:32:37 -0700

    Time: 40 ms (46.39%), Space: 16.1 MB (97.42%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,22 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        
+        def helper(n: int, k :int) -> int:
+            # print("n, k: ", n, k)
+            # assert 0 <= k < 2**n -1
+            if n == 0:
+                return 0
+            
+            checkPrev = helper(n - 1, k//2)
+            
+            dic = {
+                0: [0, 1],
+                1: [1, 0],
+            }
+            
+            return dic[checkPrev][k%2]
+        
+        ans = helper(n - 1, k - 1)
+        
+        return ans
+        
