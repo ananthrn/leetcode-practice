fd196db3272af45b334ef4416aec9a0be33a173b
Ananth Ravi Narayan <ananth360@gmail.com>
2023-12-26 03:51:31 +0530

    Time: 41 ms (48.51%), Space: 17.8 MB (5.04%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        @cache
+        def helper(s: str) -> int:
+            if s == "":
+                return 1
+            
+            if s[0] == '0':
+                return 0
+            
+            if len(s) <= 1:
+                return 1
+            
+            ans = 0
+            
+            if 1 <= int(s[:2]) <= 26:
+                ans += helper(s[2:])
+            ans += helper(s[1:])
+            
+            # print("s:", s)
+            # print("ans:", ans)
+            # print()
+            return ans
+        
+        return helper(s)
