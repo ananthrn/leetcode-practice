9daf9b24f584e19ea187d2bc71cc26dcc427b0c5
Ananth Ravi Narayan <ananth360@gmail.com>
2024-01-30 08:30:50 +0530

    Time: 70 ms (50.65%), Space: 17.1 MB (71.20%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,25 +1,30 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
         
-        ops = {
+        sign = lambda a: -1 if a < 0 else 1
+        
+        opMap = {
             "+": lambda a, b: a + b,
             "-": lambda a, b: a - b,
             "*": lambda a, b: a * b,
-            "/": lambda a, b: int(a/b)
+            "/": lambda a, b: sign(a) * sign(b) * (abs(a) // abs(b)),
         }
         
+        stack = []
+        
         for token in tokens:
-            # print("stack: ", stack)
-            # print("token: ", token)
-            if token in ops:
-                operand_2 = stack.pop()
-                operand_1 = stack.pop()
-                result = ops[token](operand_1, operand_2)
-                stack.append(result)
+            if token in opMap:
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+                
+                stack.append(opMap[token](operand2, operand1))
+                
+#                 print("token: ", token)
+                
+#                 print(f"result of {operand2} {token} {operand1}:", opMap[token](operand2, operand1))
             else:
                 stack.append(int(token))
             
-            # print("stack after:", stack)
-        return stack[0]
-                
+            # print("stack: ", stack)
+        
+        return stack[-1]
