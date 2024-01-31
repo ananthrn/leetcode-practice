a54b71be2dde25a319eba73dcaa7387eee8b999c
Ananth Ravi Narayan <ananth360@gmail.com>
2024-01-31 12:06:42 +0530

    Time: 879 ms (89.26%), Space: 30.5 MB (92.24%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -6,14 +6,11 @@ class Solution:
         
         for index, temp in enumerate(temperatures):
             # print(stack)
-            if not stack or stack[-1][0] >= temp:
-                stack.append((temp, index))
-            else:
-                while stack and stack[-1][0] < temp:
-                    popTemp, popIndex = stack.pop()
-                    ans[popIndex] = index - popIndex
+            while stack and stack[-1][0] < temp:
+                popTemp, popIndex = stack.pop()
+                ans[popIndex] = index - popIndex
                     
-                stack.append((temp, index))
+            stack.append((temp, index))
                 
                 
         
