a5752e05dac3bc536cdbb11aaef7204ace3dd7bb
Ananth Ravi Narayan <ananth360@gmail.com>
2024-02-11 13:12:58 -0800

    Time: 1410 ms (32.24%), Space: 60.1 MB (6.85%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        @cache
+        def helper(row, robot_1, robot_2) -> int:
+            if row == m or not (0 <= robot_1 <= n - 1) or not (0 <= robot_2 <= n - 1):
+                return 0
+            
+
+            mx = 0
+            for robot_1_next in range(robot_1 - 1, robot_1 + 2):
+                for robot_2_next in range(robot_2 - 1, robot_2 + 2):
+                    mx = max(mx, helper(row + 1, robot_1_next, robot_2_next))
+            
+            if robot_1 == robot_2:
+                print("row, robot_1, robot_2: ", row, robot_1, robot_2)
+                print("ans: ", mx + grid[row][robot_1])
+                print()
+                return mx + grid[row][robot_1]
+            else:
+                print("row, robot_1, robot_2: ", row, robot_1, robot_2)
+                print("ans: ", mx + grid[row][robot_1] + grid[row][robot_2])
+                print()
+                return mx + grid[row][robot_1] + grid[row][robot_2]
+        
+        m, n = len(grid), len(grid[0])
+        
+        return helper(0, 0, n - 1)
