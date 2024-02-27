f8e4fb5148e3d4e551999718eac062994b0b3d0f
Ananth Ravi Narayan <ananth360@gmail.com>
2024-02-27 12:25:00 -0800

    Time: 423 ms (83.25%), Space: 31.8 MB (43.90%) - LeetHub

--- 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.py
+++ 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.py
@@ -13,12 +13,20 @@ class Solution:
         currentAnswer = 0
         
         for i in range(n):
+            
+            while stack and stack[-1][1] <= i:
+                stack.pop()
+                
             if nums[i] > currentMax:
                 
                 currentMax = nums[i]
                 
                 print(f"i: {i}, nums[i]: {nums[i]}")
                 print(f"Before stack: {stack}")
+                
+                while stack and stack[-1][0] < nums[i]:
+                    currentAnswer = max(currentAnswer, stack[-1][1] - i  + 1)
+                    stack.pop()
 #                 while stack and (stack[-1][0] < nums[i] or stack[-1][1] <= i):
                     
 #                     if stack[-1][0] < nums[i] and i < stack[-1][1]:
@@ -26,12 +34,12 @@ class Solution:
                         
 #                     stack.pop()
                 
-                stackMax = max([stack[j][1] - i  + 1 for j in range(len(stack)) if stack[j][0] < nums[i]], default=0)       
+#                 stackMax = max([stack[j][1] - i  + 1 for j in range(len(stack)) if stack[j][0] < nums[i]], default=0)       
                 
-                currentAnswer = max(currentAnswer, stackMax)
+#                 currentAnswer = max(currentAnswer, stackMax)
                 
                 print(f"After stack: {stack}")
-                print(f"stackMax: {stackMax}")
+                # print(f"stackMax: {stackMax}")
                 print(f"currentAnswer: {currentAnswer}")
                 print()
         
