ab7a12d1c63655530d5c6a7a7d9e7ba2d99ec83b
Ananth Ravi Narayan <ananth360@gmail.com>
2024-02-28 18:39:35 -0800

    Time: 6465 ms (6.08%), Space: 245.4 MB (20.44%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.py
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.py
@@ -2,21 +2,36 @@ import sortedcontainers
 class Solution:
     def minSessions(self, tasks: List[int], sessionTime: int) -> int:
         
+        
+        def clearBit(mask: int, bit: int) -> int:
+            return ~(1 << bit) & mask
+        
+        def checkBitSet(mask: int, bit: int) -> bool:
+            val = ((mask >> bit) & 1) 
+            return val == 1
+        
         @lru_cache(None)
-        def getNumSessions(tasksRemaining: Set[int], remainingTime: int) -> int:
-            if len(tasksRemaining) == 0:
+        def getNumSessions(tasksRemaining: int, remainingTime: int) -> int:
+            if tasksRemaining == 0:
                 return 0
             
+            # print("tasksRemaining: ", tasksRemaining)
+            # print("remainingTime: ", remainingTime)
+            
             ans = len(tasks)
-            for taskIndex in tasksRemaining:
-                if tasks[taskIndex] <= remainingTime:
-                    ans = min(ans, getNumSessions(tasksRemaining - {taskIndex}, remainingTime - tasks[taskIndex]))
-                else:
-                    ans = min(ans, 1 + getNumSessions(tasksRemaining - {taskIndex}, sessionTime - tasks[taskIndex]))
+            for taskIndex in range(0, n):
+                # print(taskIndex)
+                newMask = clearBit(tasksRemaining, taskIndex)
+                if checkBitSet(tasksRemaining, taskIndex):
+                    if remainingTime >= tasks[taskIndex]:
+                        ans = min(ans, getNumSessions(newMask, remainingTime - tasks[taskIndex]))
+                    else:
+                        ans = min(ans, 1 + getNumSessions(newMask, sessionTime - tasks[taskIndex]))
             
             return ans
         
-        numSessions = getNumSessions(frozenset(range(len(tasks))), sessionTime)
+        n = len(tasks)
+        numSessions = getNumSessions((1 << n) - 1, sessionTime)
         
         return numSessions + 1
             
