2c20be941e5657287bed7e7f744c76324c20872d
Ananth Ravi Narayan <ananth360@gmail.com>
2024-03-14 21:02:42 -0700

    Time: 68 ms (86.38%), Space: 20.7 MB (14.33%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,27 +1,28 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        START = 1
-        END = 0
-
         events = []
-
-        for interval in intervals:
-            events.append((interval[0], START))
-            events.append((interval[1], END))
-        
-        events = sorted(events) 
-        
-        maxActiveMeetings = 0
-        activeMeetings = 0
-
-        for eventTime, eventType in events:
-            if eventType == START:
-                activeMeetings += 1
-            else:
-                activeMeetings -= 1
+        for start, end in intervals:
+            events.append(
+                (start, 1)
+            )
             
-            maxActiveMeetings = max(maxActiveMeetings, activeMeetings)
+            events.append(
+                (end, 0)
+            )
         
-        return maxActiveMeetings
-
-
+        events = sorted(events)
+        
+        numMeetings = 0
+        
+        maxMeetings = 0
+        for time, isStart in events:
+            if isStart:
+                numMeetings += 1
+            else:
+                numMeetings -= 1
+            
+            maxMeetings = max(maxMeetings, numMeetings)
+        
+        return maxMeetings
+            
+        
