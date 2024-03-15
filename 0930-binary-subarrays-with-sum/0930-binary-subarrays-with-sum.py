bc811eb3b0810c28dcf8333e1eaf5fdf918b7bd0
Ananth Ravi Narayan <ananth360@gmail.com>
2024-03-14 20:57:50 -0700

    Time: 606 ms (5.05%), Space: 18.4 MB (72.94%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,58 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        prefSums = (len(nums) + 1) * [0]
+        
+        for ind in range(1, len(prefSums)):
+            prefSums[ind] = prefSums[ind - 1] + nums[ind - 1]
+            
+        
+        def binSearchLowestHighest(start, end, sm):
+            lowestIndex = end + 1
+            left, right = start, end
+            
+            while left <= right:
+                # print("left, righ")
+                mid = (left + right)//2
+                if prefSums[mid] == sm:
+                    lowestIndex = min(lowestIndex, mid)
+                    right = mid - 1
+                elif prefSums[mid] > sm:
+                    right = mid - 1
+                elif prefSums[mid] < sm:
+                    left = mid + 1
+            
+            highestIndex = start - 1
+            left, right = start, end
+            
+            while left <= right:
+                mid = (left + right)//2
+                if prefSums[mid] == sm:
+                    highestIndex = max(highestIndex, mid)
+                    left = mid + 1
+                elif prefSums[mid] > sm:
+                    right = mid - 1
+                elif prefSums[mid] < sm:
+                    left = mid + 1
+            
+            return max(0, highestIndex - lowestIndex + 1)
+        
+        ans = 0
+        cnt = {}
+        
+        for ind in range(1, len(prefSums)):
+            prefSum = prefSums[ind]
+            if goal <= prefSum:
+                remainingSum = prefSum - goal
+                length = binSearchLowestHighest(0, ind - 1, remainingSum)
+                
+                ans += length
+        
+#         for prefSum in prefSums:
+#             remSum = prefSum - goal
+#             ans += cnt.get(remSum, 0)
+#             cnt[prefSum] = cnt.get(prefSum, 0) + 1
+        return ans
+            
+            
+                    
+            
