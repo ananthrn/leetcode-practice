0fc1444f2c86fdfc9cd125c67f21b717e0358123
Ananth Ravi Narayan <ananth360@gmail.com>
2024-03-14 22:51:10 -0700

    Time: 78 ms (60.67%), Space: 16.6 MB (59.86%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        
+        def helper(currentIndex: int, currentSet: Set[chr]) -> int:
+            if currentIndex >= len(arr):
+                return 0
+            
+            if currentIndex in ineligible or len(frozenset(arr[currentIndex]) & currentSet) > 0:
+                return helper(currentIndex + 1, currentSet)
+            
+            return max(
+                helper(currentIndex + 1, currentSet),
+                len(arr[currentIndex]) + helper(currentIndex + 1, currentSet | frozenset(arr[currentIndex]))
+            )
+        
+        ineligible = set(
+            [ 
+                index for index, string in enumerate(arr)
+                if collections.Counter(string).most_common()[0][1] > 1
+            ]
+        )
+        
+        print("ineligible: ", ineligible)
+        ans = helper(0, frozenset())
+        
+        return ans
+            
+            
+            
+        
