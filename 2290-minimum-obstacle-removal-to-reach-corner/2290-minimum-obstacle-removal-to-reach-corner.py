17b592d7d06169e0be8d2e30f289b9992bebf8c5
Ananth Ravi Narayan <ananth360@gmail.com>
2024-06-06 17:22:11 -0700

    Time: 2100 ms (87.08%), Space: 40.2 MB (88.21%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -7,41 +7,32 @@ class Solution:
         
         Q = collections.deque([(0, 0, 0)])
         
-        seen = set([])
         
-        answer = maxObstaclesPossible
-        
-        bestAnswerForCell = [
-            [maxObstaclesPossible + 1] * n for _ in range(m)
+        dist = [
+            [
+                inf
+            ] * n for _ in range(m)
         ]
+        
+        dist[0][0] = 0
         
         while Q:
             r, c, obstaclesSeen = Q.popleft()
             # print("r, c, obstaclesSeen: ", r, c, obstaclesSeen)
-            if (r, c) == (m - 1, n - 1):
-                answer = min(answer, obstaclesSeen)
-            else:
-                if (r, c, obstaclesSeen) not in seen:
-                    seen.add((r, c, obstaclesSeen))
-                    
-                    
-                    if obstaclesSeen < answer and obstaclesSeen < bestAnswerForCell[r][c]:
-                        for nxt_r, nxt_c in (
-                            (r + 1, c),
-                            (r - 1, c),
-                            (r, c + 1),
-                            (r, c - 1),
-                        ):
-                            bestAnswerForCell[r][c] = min(bestAnswerForCell[r][c], obstaclesSeen)
-                            if 0 <= nxt_r < m and 0 <= nxt_c < n:
-                                newObstacles = grid[nxt_r][nxt_c] + obstaclesSeen
-                                
-                                if (nxt_r, nxt_c, newObstacles) not in seen:
-                                    if grid[nxt_r][nxt_c] == 0:
-                                        Q.appendleft((nxt_r, nxt_c, newObstacles))
-                                    else:
-                                        Q.append((nxt_r, nxt_c, newObstacles))
-                    
+            dist[r][c] = min(obstaclesSeen, dist[r][c])
+            for nxt_r, nxt_c in (
+                (r + 1, c),
+                (r - 1, c),
+                (r, c + 1),
+                (r, c - 1),                
+            ):
+                if 0 <= nxt_r < m and 0 <= nxt_c < n and dist[nxt_r][nxt_c] == inf:
+                    newObstacles = grid[nxt_r][nxt_c] + obstaclesSeen
+                    dist[nxt_r][nxt_c] = newObstacles
+                    if grid[nxt_r][nxt_c] == 0:
+                        
+                        Q.appendleft((nxt_r, nxt_c, newObstacles))
+                    else:
+                        Q.append((nxt_r, nxt_c, newObstacles))
         
-        
-        return answer
+        return dist[-1][-1]
