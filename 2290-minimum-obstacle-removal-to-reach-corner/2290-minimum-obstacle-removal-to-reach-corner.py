50fb795502f08947c071893ff6afc492716932ab
Ananth Ravi Narayan <ananth360@gmail.com>
2024-06-06 17:25:27 -0700

    Time: 2253 ms (73.87%), Space: 40.2 MB (88.21%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -14,25 +14,26 @@ class Solution:
             ] * n for _ in range(m)
         ]
         
-        dist[0][0] = 0
+        # dist[0][0] = 0
         
         while Q:
             r, c, obstaclesSeen = Q.popleft()
             # print("r, c, obstaclesSeen: ", r, c, obstaclesSeen)
-            dist[r][c] = min(obstaclesSeen, dist[r][c])
-            for nxt_r, nxt_c in (
-                (r + 1, c),
-                (r - 1, c),
-                (r, c + 1),
-                (r, c - 1),                
-            ):
-                if 0 <= nxt_r < m and 0 <= nxt_c < n and dist[nxt_r][nxt_c] == inf:
-                    newObstacles = grid[nxt_r][nxt_c] + obstaclesSeen
-                    dist[nxt_r][nxt_c] = newObstacles
-                    if grid[nxt_r][nxt_c] == 0:
-                        
-                        Q.appendleft((nxt_r, nxt_c, newObstacles))
-                    else:
-                        Q.append((nxt_r, nxt_c, newObstacles))
-        
+            if dist[r][c] == inf:
+                dist[r][c] = min(obstaclesSeen, dist[r][c])
+                for nxt_r, nxt_c in (
+                    (r + 1, c),
+                    (r - 1, c),
+                    (r, c + 1),
+                    (r, c - 1),                
+                ):
+                    if 0 <= nxt_r < m and 0 <= nxt_c < n and dist[nxt_r][nxt_c] == inf:
+                        newObstacles = grid[nxt_r][nxt_c] + obstaclesSeen
+                        # dist[nxt_r][nxt_c] = newObstacles
+                        if grid[nxt_r][nxt_c] == 0:
+
+                            Q.appendleft((nxt_r, nxt_c, newObstacles))
+                        else:
+                            Q.append((nxt_r, nxt_c, newObstacles))
+
         return dist[-1][-1]
