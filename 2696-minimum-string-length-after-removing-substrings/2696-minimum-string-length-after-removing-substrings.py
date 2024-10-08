3bf9ce66688113f2663c2236d13c02fae79b3a93
Ananth Ravi Narayan <ananth360@gmail.com>
2024-10-08 15:00:05 -0700

    Time: 188 ms (5.08%), Space: 16.7 MB (15.27%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        charList = list(s)
+        
+        while True:
+            newList = []
+            index = 0
+            
+            while index < len(charList) - 1:
+                if (charList[index], charList[index + 1]) == ("A", "B") or  (charList[index], charList[index + 1]) == ("C", "D"):
+                    index += 2
+                else:
+                    newList.append(charList[index])
+                    index += 1
+            
+            if index == len(charList) - 1:
+                newList.append(charList[index])
+                
+            print("charList", charList)
+            print("newList: ", newList)
+            print()
+            if len(newList) == len(charList):
+                return len(newList)
+            else:
+                charList = newList
+        
+        
+        return 0
+            
+            
